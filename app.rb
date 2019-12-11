9b85492a04f2eaf83df1944c997f6b312122217d
Jonathan Schack <jschack94@gmail.com>
2019-12-11 00:41:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	```ruby
-		get '/' do
-		  erb :index
-		end
-	``` 
-
+	get '/' do
+		erb :index
 	end
+
+	get "/info" do
+		"Testing the info page"
+	end
+
 end
